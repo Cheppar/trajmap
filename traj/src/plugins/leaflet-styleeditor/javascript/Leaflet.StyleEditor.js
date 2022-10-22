83085d5577756f5fb23551333359982c00d9ec60
Cheppar <ejioforched@gmail.com>
2022-10-22 08:31:48 +0300

    replace cylinder

--- traj/src/plugins/leaflet-styleeditor/javascript/Leaflet.StyleEditor.js
+++ traj/src/plugins/leaflet-styleeditor/javascript/Leaflet.StyleEditor.js
@@ -5,7 +5,7 @@ L.Control.StyleEditor = L.Control.extend({
         enabled: false,
         colorRamp: ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#16a085', '#27ae60', '#2980b9', '#8e44ad', '#2c3e50', '#f1c40f', '#e67e22', '#e74c3c', '#ecf0f1', '#95a5a6', '#f39c12', '#d35400', '#c0392b', '#bdc3c7', '#7f8c8d'],
         markerApi: 'http://api.tiles.mapbox.com/v3/marker/',
-        markers: ['circle-stroked', 'circle',],
+        markers: [],
         editlayers: [],
         layerGroups: [],
         openOnLeafletDraw: true,
